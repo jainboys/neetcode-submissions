b31b84ab27806c0eeebf8f21e7b6c557de2db118
jainboys <jainboys@gmail.com>
2026-06-18 21:13:44 +0530

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    res = 1
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        sequence = {}
+        def find(x):
+            if sequence[x][0] == x:
+                return sequence[x]
+            else:
+                return find(sequence[x][0])
+        
+        def union(x, y):
+            sx = find(x)
+            sy = find(y)
+            if sx and sy:
+                if x < y:
+                    sy[0] = x
+                    sx[1]=sx[1]+sy[1]
+                    if sx[1]> self.res:
+                        self.res = sx[1]
+                else:
+                    sx[0] = y
+                    sy[1]=sy[1]+sx[1]
+                    if sy[1]> self.res:
+                        self.res = sy[1]
+
+        for i in nums:
+            if i in sequence:
+                continue
+            else:
+                sequence[i] = [i, 1]
+                for x in (i-1, i+1):
+                    if x in sequence:
+                        union(x, i)
+        return self.res
+    
+    
+
